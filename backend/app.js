895858e2e55b3947f8ebab7b8fb66ee430299c0d
jinyeol10 <zjue3000@naver.com>
2023-02-04 14:22:58 +0900

    0204

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 
 var app = express();
+
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const options = {
@@ -23,6 +25,9 @@ const options = {
 };
 const openapiSpec = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
+
+var cors = require('cors');
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
